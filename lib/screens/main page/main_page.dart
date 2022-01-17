3d667a381eee622738197d9516c3078b86c1904f
rafaucoin <88778777+rafaucoin@users.noreply.github.com>
2022-01-17 01:46:48 +0100

    First-Commit

--- lib/screens/main page/main_page.dart
+++ lib/screens/main page/main_page.dart
@@ -11,12 +11,94 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          "main page" ,
-        ),
+    return MaterialApp(
+        home: Scaffold(
+            body: SafeArea(
+                child: Container(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        children: [
+          Card(
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30)),
+            ),
+            child: Container(
+              height: 300,
+              decoration: BoxDecoration(
+                  color: Color(0xff001219),
+                  borderRadius: BorderRadius.all(Radius.circular(30))),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(children: [
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 20),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.brown.shade800,
+                          ),
+                        ),
+                        Container(
+                          child: Text(
+                            "Username",
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ]),
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.brown.shade800,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(
+                      'Where do \n you want to start ?',
+                      style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white),
+                    ),
+                  ),
+                  Container(
+                    width: 320,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          disabledBorder: InputBorder.none,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                          hintText: 'Search for an event',
+                          hintStyle:
+                              TextStyle(fontSize: 16.0, color: Colors.black),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                          contentPadding: const EdgeInsets.only(
+                              left: 14.0, bottom: 8.0, top: 8.0),
+                          suffixIcon: Icon(Icons.search)),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
-    );
+    ))));
   }
 }
